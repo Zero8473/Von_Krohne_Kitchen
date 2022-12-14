6a242fcbaa1a06e8ae9c86cf28e028f508378205
natalie-derber <natalie.derber@itSchuleStuttgart1.onmicrosoft.com>
2022-12-14 11:00:15 +0100

    created Step

---
+++ Von_Krohne_Kitchen/Step.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Von_Krohne_Kitchen
+{
+    class Step
+    {
+        public Recipe Rec;
+
+        public int No;
+
+        public string Description;
+
+        public Step(Recipe rec, int no, string description)
+        {
+            this.Rec = rec;
+            this.No = no;
+            this.Description = description;
+        }
+    }
+}
