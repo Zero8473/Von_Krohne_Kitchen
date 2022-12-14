54c6d67bd31395b90570bd123fc9bc4677e3b19d
natalie-derber <natalie.derber@itSchuleStuttgart1.onmicrosoft.com>
2022-12-14 10:56:57 +0100

    created Ingredient

---
+++ Von_Krohne_Kitchen/Ingredient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Von_Krohne_Kitchen
+{
+    class Ingredient
+    {
+        public Recipe Rec;
+
+        public string Title;
+
+        public decimal Amount;
+
+        public int Servings;
+
+        public Ingredient(Recipe rec, string title, decimal amount, int servings)
+        {
+            this.Rec = rec;
+            this.Title = title;
+            this.Amount = amount;
+            this.Servings = servings;
+        }
+    }
+}
