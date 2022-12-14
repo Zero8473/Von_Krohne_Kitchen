c249cefd254dc9edb78e6282fba4b6f319a11978
natalie-derber <natalie.derber@itSchuleStuttgart1.onmicrosoft.com>
2022-12-14 10:48:30 +0100

    created Recipe

---
+++ Von_Krohne_Kitchen/Recipe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Von_Krohne_Kitchen
+{
+    class Recipe
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        private string _pic;
+        public string Pic
+        {
+            get { return _pic; }
+            set { _pic = value; }
+        }
+
+        public List<Category> Categories = new List<Category>();
+
+        public int Servings;
+
+        public int TimeInMinutes;
+
+        public Recipe(string title, string pic)
+        {
+            this._title = title;
+            this._pic = pic;
+        }
+
+        public void AddCategory(Category category)
+        {
+            Categories.Add(category);
+        }
+    }
+}
